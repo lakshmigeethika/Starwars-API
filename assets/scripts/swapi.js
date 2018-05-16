19b246090d5bf7ba6c5985a2ef949b9648b76650
Lakshmi Geethika Tripuramallu <geethika.tripuramallu@gmail.com>
2018-05-16 08:14:46 -0400

    made changes to add species in the form

--- assets/scripts/swapi.js
+++ assets/scripts/swapi.js
@@ -1,43 +1,46 @@
-window.onload = function(){ 
-    
-document.getElementById("submit").addEventListener("click", function(){
-    var num_people = parseInt(document.getElementById("people").value);
-    console.log("number of poeple in form is " + num_people);    
-    if(!Number.isInteger(num_people)){
-       alert(" Please enter a valid number")
-    };
-     
-     
-    var request = new XMLHttpRequest();
+document.getElementById("getData").addEventListener("click", getData);
+function getData() {
+  var num_people = parseInt(document.getElementById("people").value);
+  console.log("number of poeple in form is " + num_people);
+  if (!Number.isInteger(num_people)) {
+    alert(" Please enter a valid number");
+  } else {
     var url = "https://swapi.co/api/people/";
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        var response = JSON.parse(this.responseText);
-          populateForm(response);
-      }
-    }
-    request.open("GET", url, true);
-    request.send();
-    function populateForm(response){
-       
-        for(i=0;i<num_people;i++){
-        var div = document.createElement("div");
-            div.className = "person-info"
-            div.innerHTML = "<div class='form-group'><label class='name'>Name: </label><input type='text' name='name' value=' "+response.results[i].name+"'/></div><div class='height form-group'><label class='height'>Height: </label><input type='text' name='height' value=' "+response.results[i].height+ "'/></div><div class='birth_year form-group'><label class='birth_year'>Year of Birth: </label><input type='text' name='height' value='"+ response.results[i].birth_year+ "'/></div>";
-            
-     
-            document.getElementById("people-info").appendChild(div);
-            
-            
-            
-
-        
-        console.log("in the function and the response is " + (i+1) + " th person" + response.results[i].name,response.results[i].height, response.results[i].birth_year, response.results[i].species[0] );
-     }
-    }
-});
+    fetch(url)
+      .then(resp => resp.json()) // Transform the data into json
+      .then(function(data) {
+        console.log(data);
+        var resp = data;
+        populateForm(resp, num_people);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 }
-     
-     
- 
 
+function populateForm(response, num_people) {
+  for (i = 0; i < num_people; i++) {
+    fetch(response.results[i].species[0])
+      .then(resp => resp.json()) // Transform the data into json
+      .then(function(data) {
+        var div = document.createElement("div");
+        div.className = "person-info";
+        div.innerHTML =
+          "<div class='form-group'><label class='name'>Name: </label><input type='text' name='name' value=' " +
+          response.results[i].name +
+          "'/></div><div class='height form-group'><label class='height'>Height: </label><input type='text' name='height' value=' " +
+          response.results[i].height +
+          "'/></div><div class='birth_year form-group'><label class='birth_year'>Year of Birth: </label><input type='text' name='height' value='" +
+          response.results[i].birth_year +
+          "'/></div><div class='species form-group'><label class='species'>Species: </label><input type='text' name='height' value='" +
+          data.name +
+          "' /></div>";
+
+        document.getElementById("people-info").appendChild(div);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+}
