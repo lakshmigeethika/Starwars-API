e3458db0c1a4aea4aa8bfc66a5d340783234a18d
Lakshmi Geethika Tripuramallu <geethika.tripuramallu@gmail.com>
2018-05-16 15:59:08 -0400

    finished bonus question to display species

--- assets/scripts/swapi.js
+++ assets/scripts/swapi.js
@@ -1,52 +1,54 @@
-window.onload = function(){ 
-    document.getElementById("submit").addEventListener("click", function(){
-        var num_people = parseInt(document.getElementById("people").value);
-        console.log("number of poeple in form is " + num_people);    
-        if(!Number.isInteger(num_people)){
-            alert(" Please enter a valid number")
-        };
-        var request = new XMLHttpRequest();
+document.getElementById("submit").addEventListener("click", getData);
+var getNames = [];
+var setData = [];
+function getData() {
+    document.getElementById("people-info").innerHTML = "";
+    var num_people = parseInt(document.getElementById("people").value);
+    console.log("number of poeple in form is " + num_people);
+    if (!Number.isInteger(num_people)) {
+        alert(" Please enter a valid number");
+    } else {
         var url = "https://swapi.co/api/people/";
-        request.onreadystatechange = function() {
-            if (this.readyState === 4 && this.status === 200) {
-                var response = JSON.parse(this.responseText);
-                populateForm(response);
-            }
-        }
-        request.open("GET", url, true);
-        request.send();
-        function populateForm(response){
-            
-            for(i=0;i<num_people;i++){
-              
-                
-                var div = document.createElement("div");
-                div.className = "person-info"
-                div.innerHTML = "<div class='form-group'><label class='name'>Name: </label><input type='text' name='name' value=' "+response.results[i].name+"'/></div><div class='height form-group'><label class='height'>Height: </label><input type='text' name='height' value=' "+response.results[i].height+ "'/></div><div class='birth_year form-group'><label class='birth_year'>Year of Birth: </label><input type='text' name='height' value='"+ response.results[i].birth_year+ "'/></div>"
-                
-                
-                
-                  var speciesURL = response.results[i].species[0];
-                var speciesRequest = new XMLHttpRequest();
-                 speciesRequest.onreadystatechange = function() {
-                if (this.readyState === 4 && this.status === 200) {
-                var speciesresponse = JSON.parse(this.responseText);
-                    speciesName(speciesresponse);
-                    
-                    }
-                 }
-                  speciesRequest.open("GET", speciesURL, true);
-                    speciesRequest.send();
-                function speciesName(response){
-                    console.log("response is"+ response.name);
-                    document.getElementById("species").innerHTML = response.name;
-                }
-                
-                document.getElementById("people-info").appendChild(div);
-                
-                
-                
-            }
-        }
+        fetch(url)
+            .then(resp => resp.json()) // Transform the data into json
+            .then(function(data) {
+            console.log(data);
+            var resp = data;
+            populateForm(resp, num_people, check);
+        })
+            .catch(function(error) {
+            console.log(error);
+        });
+    }
+}
+var check = function(url, response, i) {
+    var div = document.createElement("div");
+    div.className = "person-info";
+    div.innerHTML =
+        "<div class='form-group'><label class='name'>Name: </label><input type='text' name='name' value=' " +
+        response.results[i].name +
+        "'/></div><div class='height form-group'><label class='height'>Height: </label><input type='text' name='height' value=' " +
+        response.results[i].height +
+        "'/></div><div id='birth' class='birth_year form-group'><label class='birth_year'>Year of Birth: </label><input type='text' name='height' value='" +
+        response.results[i].birth_year +
+        "'/></div>" +
+        "<div class='species form-group'><label class='species'>Species: </label><input type='text' id='" +
+        i +
+        "'  /></div>";
+    document.getElementById("people-info").appendChild(div);
+    fetch(url)
+        .then(resp => resp.json()) 
+        .then(function(data) {
+        console.log(div.className);
+        document.getElementById(i).value = data.name;
+    })
+        .catch(function(error) {
+        console.log(error);
     });
+};
+function populateForm(response, num_people, callback) {
+    for (i = 0; i < num_people; i++) {
+        console.log("ie " + i + " url " + response.results[i].species[0]);
+        callback(response.results[i].species[0], response, i);
+    }
 }
